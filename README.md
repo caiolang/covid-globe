2ffbb4e5f8c73fbd60ed8856b09a83b9d5c0cc21
Caio Lang <caio.lang2@gmail.com>
2022-11-01 18:05:29 +0100

    Mudar altura e cor de um pais de acordo com dados do geojson

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 Visualization of COVID data using globe.gl
 
 https://github.com/vasturiano/globe.gl
+
+
+- Resolution
+- Altitude and color to portray num of cases
+
+TODO
+- ✅🦦 Como fazer um país ficar mais alto, com colunas
+- 🦦 Usar as mesmas cores pra cada país
+- 🦆 Como usar os dados de casos para modular a visualização
+- Como ler os dados ao longo do tempo
+- Animação
--- countries.js
+++ countries.js
@@ -1,19 +1,44 @@
-fetch("./datasets/ne_110m_admin_0_countries.geojson")
+let myGlobe;
+let btn = document.getElementById("btn");
+
+btn.onclick = () => {
+  myGlobe
+    .hexPolygonAltitude(({ properties: d }) =>
+      d.cases ? 0.25 : Math.random() / 1000 + 0.001
+    )
+    .hexPolygonColor(({ properties: d }) => {
+      let color = `#${Math.round(Math.random() * Math.pow(2, 24))
+        .toString(16)
+        .padStart(6, "0")}${percentToHex(d.cases ? 100 : 20)}`;
+      return color;
+    });
+};
+
+const percentToHex = (p) => {
+  const percent = Math.max(0, Math.min(100, p)); // bound percent from 0 to 100
+  const intValue = Math.round((percent / 100) * 255); // map percent to nearest integer (0 - 255)
+  const hexValue = intValue.toString(16); // get hexadecimal representation
+  return hexValue.padStart(2, "0").toUpperCase(); // format with leading 0 and upper case characters
+};
+
+fetch("./datasets/countries_covid.geojson")
   .then((res) => res.json())
   .then((countries) => {
-    const world = Globe()
+    myGlobe = Globe()
       .globeImageUrl("//unpkg.com/three-globe/example/img/earth-dark.jpg")
-      .hexBinPointsData(countries.features)
       .hexPolygonsData(countries.features)
-      .hexPolygonResolution(3)
+      .hexPolygonResolution(2)
       .hexPolygonMargin(0.3)
-      .hexPolygonAltitude(0.2)
-      .hexPolygonColor(
-        () =>
-          `#${Math.round(Math.random() * Math.pow(2, 24))
-            .toString(16)
-            .padStart(6, "0")}`
-      )
+      .hexPolygonsTransitionDuration(500)
+      //   .hexPolygonAltitude(({ properties: d }) =>
+      //     d.cases ? 0.25 : Math.random() / 1000 + 0.001
+      //   )
+      .hexPolygonColor(({ properties: d }) => {
+        let color = `#${Math.round(Math.random() * Math.pow(2, 24))
+          .toString(16)
+          .padStart(6, "0")}${percentToHex(20)}`;
+        return color;
+      })
       .hexPolygonLabel(
         ({ properties: d }) => `
             <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
