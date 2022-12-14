1a28ff6847f760594f683bafa78ed99f5a5df89e
Bruno Sanches <brunosanches1@outlook.com>
2022-12-14 15:41:14 +0100

    Improve code structure

--- countries.js
+++ countries.js
@@ -28,73 +28,64 @@ fetch("./datasets/colors.json")
     colorCodes = colors;
   });
 
-// fetch("./datasets/countries.geojson")
-//   .then((res) => res.json())
-//   .then((countries) => {
-//       myGlobe = Globe()
-//         .globeImageUrl("//unpkg.com/three-globe/example/img/earth-dark.jpg")
-//         .hexPolygonsData(countries.features)
-//         .hexPolygonResolution(3)
-//         .hexPolygonMargin(0.3)
-//         .hexPolygonsTransitionDuration(500)
-//         .hexPolygonColor(({ properties: d }) => {
-//           // console.log(colorCodes[d.ISO_A2]);
-//           return colorCodes[d.ISO_A2];
-//         })
-//         .hexPolygonLabel(
-//           ({ properties: d }) => `
-//           <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
-//           Population: <i>${d.POP_EST}</i>
-//         `
-//         )(document.getElementById("globeViz"));
-//   });
+function create_vizualisation(data) {
 
-  d3.json('./datasets/countries_centroids.geojson')
-    .then(countries_centroid => {
-      let date = '2021-01-03';
-
-      d3.csv('./datasets/WHO-COVID-19-global-data.csv')
-        .then((data) => {
-          data = data.filter((el) => el.Date_reported == date)
-
-          countries_centroid.features.map(function (elem) {
-            let value = data.find((v) => v.Country_code == elem.properties.ISO)
-            if (value !== undefined) {
-              elem.properties.NEW_CASES = value.New_cases
-              elem.properties.CUMULATIVE_CASES = value.Cumulative_cases
-              elem.properties.NEW_DEATHS = value.New_deaths
-              elem.properties.CUMULATIVE_DEATHS = value.Cumulative_deaths
-            }
-            else {
-              return undefined
-            }
-
-            elem.lng = elem.geometry.coordinates[0]
-            elem.lat = elem.geometry.coordinates[1]
-
-            return elem
-          })
-
-          countries_centroid.features.filter((v) => { return v !== undefined })
-          
-          console.log(countries_centroid.features.filter((d) => d.properties.ISO == 'FR'))
-          // myGlobe
-            
-
-          myGlobe = Globe()
-            .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
-            .pointsData(countries_centroid.features)
-            .pointAltitude(
-              ({properties: d}) => Math.log10(d.NEW_CASES + 1)/ Math.log10(100000000) + 0.01
-            )
-            .pointColor(({ properties: d }) => {
-              let color = colorCodes[d.ISO];
-              if (color === undefined) { return undefined; }
-              color = `${color.slice(0, -2)}${percentToHex(d.NEW_CASES ? 100 : 20)}`;
-              return color;
-            })
-            (document.getElementById("globeViz"));;
-          
-        })
-      // console.log(data)
+  myGlobe = Globe()
+    .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
+    .pointsData(data.features)
+    .pointAltitude(
+      ({properties: d}) => Math.log10(d.NEW_CASES + 1)/ Math.log10(100000000) + 0.01
+    )
+    .pointColor(({ properties: d }) => {
+      let color = colorCodes[d.ISO];
+      if (color === undefined) { return undefined; }
+      color = `${color.slice(0, -2)}${percentToHex(d.NEW_CASES ? 100 : 20)}`;
+      return color;
     })
+    .pointLabel(({ properties: d}) => `Country: ${d.COUNTRY}\nNew cases: ${d.NEW_CASES}\nCumulative cases: ${d.CUMULATIVE_CASES}\n
+      New deaths: ${d.NEW_DEATHS}\nCumulative deaths: ${d.CUMULATIVE_DEATHS}`)
+    (document.getElementById("globeViz"));;
+}
+
+function init() {
+  countries_centroid_promise = d3.json('./datasets/countries_centroids.geojson')
+  covid_data_promise = d3.csv('./datasets/WHO-COVID-19-global-data.csv')
+
+  Promise.all([countries_centroid_promise, covid_data_promise])
+    .then((data) => {
+      countries_centroid = data[0]
+      covid_data = data[1]
+
+
+      let date = '2021-01-03';
+      covid_data_filtered = covid_data.filter((el) => el.Date_reported == date)
+
+      countries_centroid.features.map(function(country) {
+        let value = covid_data_filtered.find((v) => v.Country_code == country.properties.ISO)
+        if (value !== undefined) {
+          country.properties.NEW_CASES = value.New_cases
+          country.properties.CUMULATIVE_CASES = value.Cumulative_cases
+          country.properties.NEW_DEATHS = value.New_deaths
+          country.properties.CUMULATIVE_DEATHS = value.Cumulative_deaths
+        }
+        else {
+          return undefined
+        }
+
+        country.lng = country.geometry.coordinates[0]
+        country.lat = country.geometry.coordinates[1]
+
+        return country
+      })
+
+      countries_centroid.features = countries_centroid.features.filter((v) => { return v !== undefined })
+
+      create_vizualisation(countries_centroid)
+
+    })
+}
+
+window.addEventListener('load', init);
+
+
+
