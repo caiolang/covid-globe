fff287f1e9eed2fa8068509534fc7bd1c2917b39
Bruno Sanches <brunosanches1@outlook.com>
2022-12-15 23:41:43 +0100

    Create helper popover

--- countries.js
+++ countries.js
@@ -216,3 +216,10 @@ function init() {
 window.addEventListener("load", init);
 dateInput.addEventListener("input", updateVis);
 metricOption.addEventListener("input", updateVis);
+
+// Enable popovers
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
