e2624ba3c1cfb12db347411ba58bf6dcccf6e28f
Caio Lang <caio.lang2@gmail.com>
2022-12-16 02:16:23 +0100

    [Docs] Readme, Help button, GitHub button, title

--- countries.js
+++ countries.js
@@ -55,19 +55,6 @@ const dateToString = (date) => {
   return date.toISOString().split("T")[0];
 };
 
-const percentToHex = (p) => {
-  const percent = Math.max(0, Math.min(100, p)); // bound percent from 0 to 100
-  const intValue = Math.round((percent / 100) * 255); // map percent to nearest integer (0 - 255)
-  const hexValue = intValue.toString(16); // get hexadecimal representation
-  return hexValue.padStart(2, "0").toUpperCase(); // format with leading 0 and upper case characters
-};
-
-fetch("./datasets/colors.json")
-  .then((res) => res.json())
-  .then((colors) => {
-    colorCodes = colors;
-  });
-
 function altituteConversion(altitude) {
   return Math.log10(altitude + 1) / Math.log10(100000000) + 0.01;
 }
